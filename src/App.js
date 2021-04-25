5794dddc86cf1918732e997b92482d8dfb52c4df
Barış <barisayyildiz.1799@gmail.com>
2021-04-25 17:16:44 +0300

    arayüz oluşturuldu annotationlar seçilebiliyor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState, useRef} from 'react';
 
-function App() {
+import {
+  ReactPictureAnnotation,
+  defaultShapeStyle,
+  DefaultInputSection
+} from "react-picture-annotation";
+
+import './App.scss';
+import axios from 'axios';
+
+const App = () => {
+
+	const myRef = useRef();
+	
+  const [pageSize, setPageSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+
+	const [image, setImage] = useState(null);
+
+  const onResize = () => {
+    setPageSize({ width: window.innerWidth, height: window.innerHeight });
+  };
+
+  useEffect(() => {
+    // window.addEventListener('resize', onResize);
+    // return () => window.removeEventListener('resize', onResize);
+
+		setImage("https://source.unsplash.com/random/800x600");
+
+  }, []);
+
+  const onSelect = selectedId => {
+		// console.log(selectedId)
+	};
+  const onChange = data => {
+		// console.log(data)
+	};
+
+	const onClick = () => {
+
+		const data = myRef.current.currentAnnotationData.map(item => {
+			return(
+				{
+					tag : item.comment,
+					x : item.mark.x,
+					y : item.mark.y,
+					width : item.mark.width,
+					height : item.mark.height
+				}
+			)
+		})
+		// console.log(data);
+		axios.post("http://localhost:5000/photos",data)
+		.then(res => console.log(res))
+
+	}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+			<div className="wrapper">
+
+				<ReactPictureAnnotation
+					ref={myRef}
+					image = {image}
+					onSelect={onSelect}
+					onChange={onChange}
+					width={800}
+					height={600}
+				/>
+
+				<button onClick={onClick}>Submit</button>
+
+			</div>
+
     </div>
   );
-}
+};
 
 export default App;
